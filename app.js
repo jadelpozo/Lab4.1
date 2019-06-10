303bd70e00fd62b369f1948dd9272130ed01db3c
Jose <jadelpozo26@gmail.com>
2019-06-09 22:51:23 -0600

    Ultimos Cambios

--- app.js
+++ app.js
@@ -1,84 +1,84 @@
-const mysql =require('mysql');
+//use path module
+const path = require('path');
+//use express module
 const express = require('express');
-var app = express();
-const bodyparser = require('body-parser');
+//use hbs view engine
+const hbs = require('hbs');
+//use bodyParser middleware
+const bodyParser = require('body-parser');
+//use mysql database
+const mysql = require('mysql');
+const app = express();
 
-app.use(bodyparser.json());
+//const path = require('path');
 
-var mysqlConnection = mysql.createConnection({
-    host:'lab4.cpqom2m5tam2.us-east-1.rds.amazonaws.com',
+//app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, 'views'));
+
+//app.use(morgan('dev'));
+
+const conn = mysql.createConnection({
+    host: 'lab4.cpqom2m5tam2.us-east-1.rds.amazonaws.com',
     user: 'virt',
-    password : 'virtualizacion26',
+    password: 'virtualizacion26',
     database: 'PeliculaDB'
+  });
+//connect to database
+conn.connect((err) =>{
+  if(err) throw err;
+  console.log('Mysql Connected...');
 });
-
-mysqlConnection.connect((err)=>{
-    if(!err)
-        console.log('DB conexion con la base exitosa.');
-    else   
-        console.log('DB fallo la conexion \n Error: ' + JSON.stringify(err, undefined, 2));
+ 
+//set views file
+app.set('views',path.join(__dirname,'views'));
+//set view engine
+app.set('view engine', 'hbs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//set public folder as static folder for static file
+app.use('/assets',express.static(__dirname + '/public'));
+ 
+//route for homepage
+app.get('/',(req, res) => {
+  let sql = "SELECT * FROM Peli";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.render('product_view',{
+      results: results
+    });
+  });
 });
-
-
-app.listen(3000,()=>console.log('Express server is running at port no : 30000'));
-
-
-//GET peliculas
-app.get('/peliculas',(req,res)=>{
-    mysqlConnection.query('SELECT * FROM Peli', (err, rows, fields)=>{
-        if(!err)
-        res.send(rows);
-        else
-        console.log(err);
-    })
+ 
+//route for insert data
+app.post('/save',(req, res) => {
+  let data = {Nombre: req.body.Nombre, Genero: req.body.Genero};
+  let sql = "INSERT INTO Peli SET ?";
+  let query = conn.query(sql, data,(err, results) => {
+    if(err) throw err;
+    res.redirect('/');
+  });
 });
+ 
+//route for update data
+app.post('/update',(req, res) => {
+    let sql = "UPDATE Peli SET Nombre='"+req.body.Nombre+"', Genero='"+req.body.Genero+"' WHERE idPeli="+req.body.idPeli;
+    let query = conn.query(sql, (err, results) => {
+      if(err) throw err;
+      res.redirect('/');
+    });
+  });
 
-//GET pelicula
-app.get('/peliculas/:id',(req,res)=>{
-    mysqlConnection.query('SELECT * FROM Peli WHERE idPeli = ?',[req.params.id],(err, rows, fields)=>{
-        if(!err)
-        res.send(rows);
-        else
-        console.log(err);
-    })
-});
-
-//borrar pelicula
-app.delete('/peliculas/:id',(req,res)=>{
-    mysqlConnection.query('DELETE Peli WHERE idPeli = ?',[req.params.id],(err, rows, fields)=>{
-        if(!err)
-        res.send('Pelicula borrada ');
-        else
-        console.log(err);
-    })
-});
-
-//Insertar pelicula
-app.post('/peliculas',(req,res)=>{
-    let pelicula = req.body;
-    var sql = "SET @id = ?; SET  @nombre = ?; SET @genero = ?; \
-    CALL peliculaAddOrEdit(@id, @nombre, @genero);";
-    mysqlConnection.query(sql, [pelicula.id, pelicula.nombre, pelicula.genero],[req.params.id],(err, rows, fields)=>{
-        if(!err)
-            rows.forEach(element => {
-                if(element.constructor == Array)
-                res.send('Se insertó la pelicula: ' + element[0].id);
-            });
-        else
-            console.log(err);
-    })
-});
-
-
-//actualizar pelicula
-app.put('/peliculas',(req,res)=>{
-    let pelicula = req.body;
-    var sql = "SET @id = ?; SET  @nombre = ?; SET @genero = ?; \
-    CALL peliculaAddOrEdit(@id, @nombre, @genero);";
-    mysqlConnection.query(sql, [pelicula.id, pelicula.nombre, pelicula.genero],[req.params.id],(err, rows, fields)=>{
-        if(!err)
-            res.send("Se actualizó correctamente");
-        else
-            console.log(err);
-    })
+   
+  //route for delete data
+  app.post('/delete',(req, res) => {
+    let sql = "DELETE FROM PeliculaDB.Peli WHERE idPeli ="+req.body.idPeli+"";
+    let query = conn.query(sql, (err, results) => {
+      if(err) throw err;
+        res.redirect('/');
+    });
+  });
+ 
+//server listening
+app.listen(3000, () => {
+  console.log('Server is running at port 3000');
 });
